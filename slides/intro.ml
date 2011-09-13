431eef6cd6b0fdaa1fd0c74ffe0aa19aaf50a28c
Anil Madhavapeddy <anil@recoil.org>
2011-09-13 16:48:04 +0100

    sync more slides

--- slides/intro.ml
+++ slides/intro.ml
@@ -3,6 +3,8 @@ open Cow
 open Printf
 open Slides
 
+let rt = ">>" (* required to embed it in html p4 as cant put that token directly there *)
+let dl = "$"
 let slides = [
 {
   styles=[];
@@ -77,6 +79,45 @@ let slides = [
      <li><b>"Fat-free"</b>: layers of legacy software, compile out the layers for energy efficiency and performance</li>
    </ul>
     <div class="red">avsm: split this up into multiple slides with diagrams (from HotCloud paper perhaps)</div>
+  >>
+};
+{ styles=[];
+  content= <:html<
+   <h3>Hello Mirage World (i)</h3>
+   <p>Lets write some code that simply sleeps for a second, and outputs a string to the console.</p><br />
+   <div class="build">
+     <p>In a new directory, create <tt>hello.ml</tt>:</p>
+     <section><pre>
+let main () =
+  OS.Time.sleep 1.0 $str:rt$
+  OS.Console.log_s "Hello Mirage World!" $str:rt$
+  OS.Time.sleep 1.0
+</pre></section>
+     <p>Now, build a UNIX binary:</p>
+<section><pre class="noprettyprint">
+$str:dl$ mir-build unix-socket/hello.bin
+$str:dl$ ./_build/unix-socket/hello.bin
+Hello Mirage World!
+</pre></section>
+   </div>
+  >>
+};
+{ styles=[];
+  content= <:html<
+    <h3>Hello Mirage World (ii)</h3>
+    <p><tt>mir-build</tt> is a wrapper over <tt>ocamlbuild</tt>.<br />
+     Output files are in <tt>_build/</tt> and source is never modified.</p>
+    <ul>
+      <li><tt>mir-build -clean</tt> will remove all built files</li>
+      <li><tt>mir-build -j 5</tt> runs a parallel build.</li>
+    </ul>
+    <p>Mirage also has extra rules. Prepend the backend name to your target:</p>
+    <section><pre class="noprettyprint">
+$str:dl$ mir-build xen/hello.xen
+$str:dl$ mir-build node/hello.js
+$str:dl$ mir-build unix-direct/hello.bin
+$str:dl$ mir-build unix-socket/hello.bin
+</pre></section>
   >>
 }
 ]
