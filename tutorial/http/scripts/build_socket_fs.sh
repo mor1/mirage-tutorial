98bfd8e9c4927aacabe49de61602c930ca90a1de
Thomas Gazagnaire <thomas@gazagnaire.org>
2011-09-19 23:25:33 +0900

    [http] add missing kv_ro_server.mir file and fix build script

--- tutorial/http/scripts/build_socket_fs.sh
+++ tutorial/http/scripts/build_socket_fs.sh
@@ -4,4 +4,4 @@ BIN=kv_ro_server
 MIR_RUN=$(which mir-run)
 
 mir-build unix-socket/${BIN}.bin
-sudo ${MIR_RUN} -b unix-socket -kv_ro static:static ../_build/unix-socket/${BIN}.bin
+sudo ${MIR_RUN} -b unix-socket -kv_ro static:static ./_build/unix-socket/${BIN}.bin
