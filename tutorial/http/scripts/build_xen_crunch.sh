2fb9cfc500b838a671e8bde34e2b3d6be7473580
Anil Madhavapeddy <anil@recoil.org>
2011-09-19 15:49:04 +0100

    fix xen crunch script

--- tutorial/http/scripts/build_xen_crunch.sh
+++ tutorial/http/scripts/build_xen_crunch.sh
@@ -3,6 +3,6 @@
 BIN=crunch_server
 MIR_RUN=$(which mir-run)
 
-mir-crunch -name "static" static filesystem_static.ml
+mir-crunch -name "static" static > filesystem_static.ml
 mir-build xen/${BIN}.xen
 sudo ${MIR_RUN} -b xen -vif xenbr0 ./_build/xen/${BIN}.xen
