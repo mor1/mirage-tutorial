0ede48ed7bfac1f86b2038ec454d9718de40f33b
Richard Mortier <mort@cantab.net>
2015-08-31 22:09:52 +0100

    server: update to latest `Cohttp`

--- src/server.ml
+++ src/server.ml
@@ -16,7 +16,7 @@ module Main (C: CONSOLE) (STATIC: KV_RO) (S: Cohttp_lwt.Server) = struct
     in
 
     let callback conn_id req body =
-      let path = req |> S.Request.uri |> Uri.path in
+      let path = req |> Cohttp.Request.uri |> Uri.path in
       C.log c (Printf.sprintf "URL: '%s'" path);
 
       try_lwt
@@ -31,7 +31,7 @@ module Main (C: CONSOLE) (STATIC: KV_RO) (S: Cohttp_lwt.Server) = struct
         in
         match cpts with
         | [] | [""] -> S.respond_string ~status:`OK ~body:Content.body ()
-        | x -> S.respond_not_found ~uri:(S.Request.uri req) ()
+        | x -> S.respond_not_found ~uri:(Cohttp.Request.uri req) ()
     in
 
     let conn_closed (_, conn_id) =
@@ -39,6 +39,7 @@ module Main (C: CONSOLE) (STATIC: KV_RO) (S: Cohttp_lwt.Server) = struct
       C.log c (Printf.sprintf "conn %s closed" cid)
     in
 
-    http (S.make ~callback ~conn_closed ())
+    let spec = S.make ~callback ~conn_closed () in
+    http (`TCP 80) spec
 
 end
